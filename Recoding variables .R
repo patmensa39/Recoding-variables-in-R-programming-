49ec3e7c503f3faa89c0772ed9a6fee89858a3e8
Patrick Mensah <70266683+patmensa39@users.noreply.github.com>
2022-08-02 23:32:52 -0400

    Add files via upload

---
+++ Recoding variables .R
@@ -0,0 +1,38 @@
+#RECODING VARIABLES ###
+### Loading the libraries ###
+pacman::p_load(pacman, rio, tidyverse)
+
+### loading and preparing the dataset (Google search data) 
+data<- import("StateData.xlsx") %>% as_tibble() %>% select(state_code, 
+        psychRegions, instagram:modernDance) %>% 
+        mutate(psychRegions = as.factor(psychRegions))
+
+view(data) # viewing the data 
+
+### Creating a new variable called Friendly from psychRegions and making the Friendly and 
+### conventional states 'yes' and relaxed and creative 'maybe' Temperamental and Uninhibited 'no'
+### and all others 'under investigation'
+
+newdata<- data %>% mutate(Friendly = recode(psychRegions,
+                          "Friendly and Conventional" = "yes",
+                          "Relaxed and Creative" = "no", 
+                          .default = "under investigation")) %>%
+           select(state_code, psychRegions, Friendly)
+view(newdata)
+
+### Selecting multiple values and recoding them using case_when 
+data2<- data %>% mutate(Philant = case_when(instagram < 1 | facebook > 0 | retweet < 1 ~ "yes", 
+                                            TRUE ~ "no")) %>% 
+                select(state_code, instagram, facebook, retweet, Philant)
+view(data2)
+
+### Arranging the new created variable 'Philant' in descending order 
+data3<- data %>% mutate(Philant = case_when(instagram < 1 | facebook > 0 | retweet < 1 ~ "yes", 
+                                            TRUE ~ "no")) %>% 
+  select(state_code, instagram, facebook, retweet, Philant) %>% arrange(desc(Philant)) %>%
+  print(n = Inf) # n = Inf helps in printing all the values 
+
+view(data3)
+
+
+
